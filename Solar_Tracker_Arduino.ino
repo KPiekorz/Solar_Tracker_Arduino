9f2a299012f5b7b1de07bcbc9849317cd7120c34
KPiekorz <piekorz@student.agh.edu.pl>
2021-12-02 19:01:44 +0100

    update

--- Solar_Tracker_Arduino.ino
+++ Solar_Tracker_Arduino.ino
@@ -1,28 +1,73 @@
+/*
+ * Leds for signalling working state of system.
+ * Button for start and stop system.
+ * 
+ */
+
+#define STEP_MS 1000                
+
+#define DEBUG
+
+#ifdef DEBUG
+    #define LOG_DEBUG(s)      Serial.print(s);
+#else
+    #define LOG_DEBUG(s)
+#endif
+
+#define LOG_ERROR(s)          Serial.print(s);
 
 /* photo resistor analog pins */
-const int SENSOR_ORANGE_PIN = A5;
-const int SENSOR_GREEN_PIN  = A4;
-const int SENSOR_BLUE_PIN   = A3;
-const int SENSOR_WHITE_PIN  = A2;
+const int SENSOR_ORANGE_PIN = A5, SENSOR_GREEN_PIN  = A4, SENSOR_BLUE_PIN   = A3, SENSOR_WHITE_PIN  = A2;
+
+/* photoresistors values values */
+float sensor_orange, sensor_green, sensor_blue, sensor_white;
+
+/* button press state */
+bool button_pressed;
 
 void setup() {
 
   Serial.begin(9600);
 }
 
+static void read_photoresistors(void);
+static void update_state_machine(void);
+static void control_servomotors(void);
+
 void loop() {
 
-  float sensor_orange = analogRead(SENSOR_ORANGE_PIN);
-  float sensor_green = analogRead(SENSOR_GREEN_PIN);
-  float sensor_blue = analogRead(SENSOR_BLUE_PIN);
-  float sensor_white = analogRead(SENSOR_WHITE_PIN);
+  read_photoresistors();
 
+  update_state_machine();
+
+  control_servomotors();
+
+  delay(STEP_MS);
+}
+
+static void read_photoresistors(void) {
+
+  // read analog input of photoresistors
+  sensor_orange = analogRead(SENSOR_ORANGE_PIN);
+  sensor_green = analogRead(SENSOR_GREEN_PIN);
+  sensor_blue = analogRead(SENSOR_BLUE_PIN);
+  sensor_white = analogRead(SENSOR_WHITE_PIN);
+
+  // print sensors values
   String photo_resistor_output = String(" Sensor orange:"   + String(sensor_orange, DEC)  +
                                         " Sensor green:"    + String(sensor_green, DEC)   +
                                         " Sensor blue:"     + String(sensor_blue, DEC)    +
                                         " Sensor white:"    + String(sensor_white, DEC)   +
                                         "\n");
-  Serial.print(photo_resistor_output);
+  LOG_DEBUG(photo_resistor_output);
+}
 
-  delay(1000);
+static void update_state_machine(void) {
+
+  
+}
+
+static void control_servomotors(void) {
+
+  
 }
