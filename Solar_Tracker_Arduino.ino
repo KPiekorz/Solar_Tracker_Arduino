e9eb1c15b5423dda97341fd3dd5dc0eec6a183a9
KPiekorz <piekorz@student.agh.edu.pl>
2021-12-03 12:52:11 +0100

    update

--- Solar_Tracker_Arduino.ino
+++ Solar_Tracker_Arduino.ino
@@ -1,7 +1,7 @@
 /*
+ * NOTES:
  * Leds for signalling working state of system.
  * Button for start and stop system.
- *
  */
 
 #define STEP_MS             (1000)
@@ -19,6 +19,12 @@
 
 /* photo resistor analog pins */
 const int SENSOR_ORANGE_PIN = A5, SENSOR_GREEN_PIN  = A4, SENSOR_BLUE_PIN   = A3, SENSOR_WHITE_PIN  = A2;
+
+/* servos pwm pins */
+const int SERVO_AZIMUTH_PIN = 10, SERVO_ELEVATION_PIN = 11;
+
+/* leds pins */
+const int LED_AZIMUTH_PIN = 12, LED_ELEVATION_PIN = 13;
 
 typedef enum {
   INIT_STATE = 0,
@@ -40,10 +46,28 @@ typedef struct {
   bool azimuth_done;
 } solar_tracker_t;
 
-solar_tracker_t solar_tracker;
+solar_tracker_t solar_tracker = {.state = INIT_STATE,
+                                 .sensor_orange = 0, .sensor_green = 0, .sensor_blue = 0, .sensor_white = 0,
+                                 .button_pressed = false,
+                                 .azimuth_done = false
+                                };
 
 void setup() {
+  /* init serial com port */
   Serial.begin(9600);
+  /* init interrupt for button */
+  attachInterrupt(digitalPinToInterrupt(2), button_interrupt, RISING);	// Configure interrupt
+  /* init photoresistors analog */
+  pinMode(SENSOR_ORANGE_PIN, INPUT);
+  pinMode(SENSOR_GREEN_PIN, INPUT);
+  pinMode(SENSOR_ORANGE_PIN, INPUT);
+  pinMode(SENSOR_WHITE_PIN, INPUT);
+  /* init pwm for servos */
+  pinMode(SERVO_AZIMUTH_PIN, OUTPUT);
+  pinMode(SERVO_ELEVATION_PIN, OUTPUT);
+  /* init leds for signaling state */
+  pinMode(LED_AZIMUTH_PIN, OUTPUT);
+  pinMode(LED_ELEVATION_PIN, OUTPUT);
 }
 
 void loop() {
@@ -53,19 +77,26 @@ void loop() {
   delay(STEP_MS);
 }
 
-void read_photoresistors(void) {
+/* interrupt */
 
-  // read analog input of photoresistors
+void button_interrupt(void) {
+  solar_tracker.button_pressed = true;
+}
+
+/* global function */
+
+void read_photoresistors(void) {
+  /* read analog input of photoresistors */
   solar_tracker.sensor_orange = analogRead(SENSOR_ORANGE_PIN);
   solar_tracker.sensor_green = analogRead(SENSOR_GREEN_PIN);
   solar_tracker.sensor_blue = analogRead(SENSOR_BLUE_PIN);
   solar_tracker.sensor_white = analogRead(SENSOR_WHITE_PIN);
 
-  // print sensors values
+  /* print sensors values */
   LOG_DEBUG(String("Sensor orange: "   + String(solar_tracker.sensor_orange, DEC)  +
-                   " Sensor green: "    + String(solar_tracker.sensor_green, DEC)   +
-                   " Sensor blue: "     + String(solar_tracker.sensor_blue, DEC)    +
-                   " Sensor white: "    + String(solar_tracker.sensor_white, DEC)
+                   " Sensor green: "   + String(solar_tracker.sensor_green, DEC)   +
+                   " Sensor blue: "    + String(solar_tracker.sensor_blue, DEC)    +
+                   " Sensor white: "   + String(solar_tracker.sensor_white, DEC)
                   )
             );
 }
@@ -80,8 +111,20 @@ void set_state(solar_tracker_state_t state) {
   }
 }
 
-void state_machine_evolution(void) {
+void set_azimuth(void) {
+    // sensorValue = analogRead(A0);
+    // outputValue = map(sensorValue, 0, 1023, 0, 255);
+}
 
+void set_elevation(void) {
+
+}
+
+void stop_motors(void) {
+
+}
+
+void state_machine_evolution(void) {
   switch(solar_tracker.state) {
     case INIT_STATE: {
       if(solar_tracker.button_pressed) {
@@ -110,6 +153,7 @@ void state_machine_evolution(void) {
     }
     break;
     case STOP_STATE: {
+      stop_motors();
       if(solar_tracker.button_pressed) {
         set_state(AZIMUTH_CONTROL_STATE);
       }
@@ -121,10 +165,9 @@ void state_machine_evolution(void) {
   }
 
   /* clear button pressed */
-  solar_tracker.button_pressed = FALSE;
+  solar_tracker.button_pressed = false;
 }
 
 void control_servomotors(void) {
-
 
 }
